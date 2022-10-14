30fded9935f0865a3823252116d4254f2ae358d6
Antros Economides <ae457@cam.ac.uk>
2022-10-14 12:46:24 +0100

    Implement and test QPSK

--- modulation.py
+++ modulation.py
@@ -14,6 +14,15 @@ class Modulator(Component):
         assert data.ndim == 1
 
 
+class Demodulator(Component):
+    input_type = "cd symbols"
+    output_type = "u8 data"
+
+    @abstractmethod
+    def __call__(self, symbols: np.ndarray) -> np.ndarray:
+        assert symbols.ndim == 1
+
+
 class ModulatorBPSK(Modulator):
     def __call__(self, data: np.ndarray) -> np.ndarray:
         super().__call__(data)
@@ -25,17 +34,45 @@ class ModulatorBPSK(Modulator):
         return 1 - 2 * data.astype(np.cdouble)
 
 
-class Demodulator(Component):
-    input_type = "cd symbols"
-    output_type = "u8 data"
-
-    @abstractmethod
-    def __call__(self, symbols: np.ndarray) -> np.ndarray:
-        assert symbols.ndim == 1
-
-
 class DemodulatorBPSK(Demodulator):
     def __call__(self, symbols: np.ndarray) -> np.ndarray:
         super().__call__(symbols)
 
         return (symbols < 0).astype(np.uint8)
+
+
+class ModulatorQPSK(Modulator):
+    def __call__(self, data: np.ndarray) -> np.ndarray:
+        super().__call__(data)
+
+        assert data.min() >= 0
+        assert data.max() <= 3
+
+        # Constellation has 4 symbols. Adjacent symbols only vary by 1 bit.
+        #         Q
+        #   01    |    00
+        #         |
+        #  ---------------I
+        #         |
+        #   11    |    10
+        #         |
+        I = 1 - 2 * (data & 0b01).astype(np.bool8)
+        Q = 1j - 2j * (data & 0b10).astype(np.bool8)
+
+        print(I)
+        print(Q)
+
+        # Normalize symbol energy.
+        return (I + Q) / np.sqrt(2)
+
+
+class DemodulatorQPSK(Demodulator):
+    def __call__(self, symbols: np.ndarray) -> np.ndarray:
+        super().__call__(symbols)
+
+        # In-phase component is the LSB.
+        data = (symbols.real < 0).astype(np.uint8)
+        # Quadrature component is the MSB.
+        data |= (symbols.imag < 0).astype(np.uint8) << 1
+
+        return data
--- test/test_modulation.py
+++ test/test_modulation.py
@@ -1,6 +1,6 @@
-import pytest
 import numpy as np
-from modulation import DemodulatorBPSK, ModulatorBPSK
+import pytest
+from modulation import DemodulatorBPSK, DemodulatorQPSK, ModulatorBPSK, ModulatorQPSK
 
 
 class TestModulatorBPSK:
@@ -16,16 +16,19 @@ class TestModulatorBPSK:
         self.modulator(np.asarray((0, 1)))
 
     def test_mapping(self):
-        data = np.asarray((0, 1))
+        data = np.arange(2, dtype=np.uint8)
         symbols = self.modulator(data)
+
+        # Check dtype.
+        assert symbols.dtype == np.cdouble
 
         # Length should be preserved.
         assert data.size == symbols.size
 
-        # 0 must be mapped to 1
+        # 0 must be mapped to 1.
         assert symbols[0] == 1
 
-        # 1 must be mapped to -1
+        # 1 must be mapped to -1.
         assert symbols[1] == -1
 
 
@@ -33,8 +36,11 @@ class TestDemodulatorBPSK:
     demodulator = DemodulatorBPSK()
 
     def test_demodulator(self):
-        symbols = np.asarray((-1, -0.5, 0.5, 1))
+        symbols = np.asarray((-1, -0.5, 0.5, 1), dtype=np.cdouble)
         data = self.demodulator(symbols)
+
+        # Check dtype.
+        assert data.dtype == np.uint8
 
         # Length should be preserved.
         assert symbols.size == data.size
@@ -60,3 +66,80 @@ class TestDemodulatorBPSK:
         # Small amounts of noise should not introduce any errors.
         noise = rng.uniform(-0.5, 0.5, size=LENGTH)
         assert np.all(self.demodulator(modulator(data) + noise) == data)
+
+
+class TestModulatorQPSK:
+    modulator = ModulatorQPSK()
+
+    def test_bounds_checking(self):
+        with pytest.raises(Exception):
+            self.modulator(np.asarray(-1))
+        with pytest.raises(Exception):
+            self.modulator(np.asarray(4))
+
+        # Only 2 bit numbers are allowed.
+        self.modulator(np.asarray((0, 1, 2, 3)))
+
+    def test_mapping(self):
+        data = np.arange(4, dtype=np.uint8)
+        symbols = self.modulator(data)
+
+        # Check dtype.
+        assert symbols.dtype == np.cdouble
+
+        # Length should be preserved.
+        assert data.size == symbols.size
+
+        # All symbols must have unit energy.
+        assert np.allclose(np.abs(symbols), 1)
+
+        # Un-normalize energy to make the comparisons below easier.
+        symbols *= np.sqrt(2)
+
+        # Check constellation.
+        #         Q
+        #   01    |    00
+        #         |
+        #  ---------------I
+        #         |
+        #   11    |    10
+        #         |
+        assert symbols[0] == 1 + 1j
+        assert symbols[1] == -1 + 1j
+        assert symbols[2] == 1 - 1j
+        assert symbols[3] == -1 - 1j
+
+
+class TestDemodulatorQPSK:
+    demodulator = DemodulatorQPSK()
+
+    def test_demodulator(self):
+        symbols = np.asarray((1 + 1j, -1 + 1j, 1 - 1j, -1 - 1j), dtype=np.cdouble)
+        data = self.demodulator(symbols)
+
+        # Check dtype.
+        assert data.dtype == np.uint8
+
+        # Length should be preserved.
+        assert symbols.size == data.size
+
+        # Check that symbols have been demodulated correctly.
+        assert data[0] == 0b00
+        assert data[1] == 0b01
+        assert data[2] == 0b10
+        assert data[3] == 0b11
+
+    def test_combined(self):
+        LENGTH = 1024
+
+        modulator = ModulatorQPSK()
+        rng = np.random.default_rng()
+
+        # Modulation should be reversible.
+        data = rng.integers(0, 3, endpoint=True, size=LENGTH)
+        assert np.all(self.demodulator(modulator(data)) == data)
+
+        # Small amounts of noise should not introduce any errors.
+        noise_r = rng.uniform(-0.5, 0.5, size=LENGTH)
+        noise_i = rng.uniform(-0.5, 0.5, size=LENGTH) * 1j
+        assert np.all(self.demodulator(modulator(data) + noise_r + noise_i) == data)
