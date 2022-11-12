537d87128f343597a7f7dc9191c00359f2242032
Antros Economides <ae457@cam.ac.uk>
2022-11-12 22:53:35 +0000

    Attempt to implement shot noise

--- receiver.py
+++ receiver.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.typing import NDArray
 from scipy.constants import Planck, elementary_charge, speed_of_light
 
-from utils import Component, signal_energy, power_dbm_to_lin
+from utils import Component, signal_power, power_dbm_to_lin
 
 
 class OpticalFrontEnd(Component):
@@ -12,7 +12,7 @@ class OpticalFrontEnd(Component):
     output_type = "cd symbols"
 
     lo_power = power_dbm_to_lin(10)  # 10 dBm is the max for Class 1 lasers.
-    lo_amplitude = np.sqrt(lo_power)  # Signal power is the mean sample squared.
+    lo_amplitude = np.sqrt(2 * lo_power)  # Signal power to peak amplitude.
 
     @cached_property
     def responsivity(self) -> float:
@@ -45,65 +45,28 @@ class NoisyOpticalFrontEnd(OpticalFrontEnd):
         assert sampling_rate > 0
         self.sampling_rate = sampling_rate
 
-        print(rx_power_dbm)
         self.rx_power = power_dbm_to_lin(rx_power_dbm)
-
-    @classmethod
-    def energy_to_photons(cls, energies: NDArray[np.float64]) -> NDArray[np.float64]:
-        # Divide each sample energy by hf.
-        return cls.WAVELENGTH / (Planck * speed_of_light) * energies
-
-    @classmethod
-    def photons_to_current(cls, photons: NDArray[np.int64]) -> NDArray[np.float64]:
-        # Multiply each sample by hf.
-        return Planck * speed_of_light / cls.WAVELENGTH * photons
-
-    def photodiode_incident_energy(
-        self, A_r: NDArray[np.float64], phi_r: NDArray[np.float64], lo_phase: float
-    ) -> NDArray[np.float64]:
-        energies = (
-            A_r**2
-            + self.lo_amplitude**2
-            + 2 * A_r * self.lo_amplitude * np.cos(phi_r - lo_phase)
-        )
-
-        return energies / 4
-
-    def photodiode_current(
-        self, A_r: NDArray[np.float64], phi_r: NDArray[np.float64], lo_phase: float
-    ) -> NDArray[np.float64]:
-        energies = self.photodiode_incident_energy(A_r, phi_r, lo_phase)
-        photons = self.energy_to_photons(self.responsivity * energies)
-
-        # FIXME switch to a normal distribution and stop converting to and from
-        # photons.
-        noisy_photons = np.random.poisson(photons)
-        currents = self.photons_to_current(noisy_photons)
-
-        # Calculate SNR.
-        signal = signal_energy(photons)
-        noise = signal_energy(photons - noisy_photons)
-        print("SNR", 10 * np.log10(signal / noise), "dB")
-
-        return currents
+        self.rng = np.random.default_rng()
 
     def __call__(self, Efields: NDArray[np.cdouble]) -> NDArray[np.cdouble]:
         # Set received signal power to that specified.
         # TODO remove this once we have proper amplitudes upstream.
-        target_energy = self.rx_power * Efields.size / self.sampling_rate
-        Efields *= np.sqrt(target_energy / signal_energy(Efields))
+        Efields *= np.sqrt(self.rx_power / signal_power(Efields))
+        assert np.isclose(signal_power(Efields), self.rx_power)
 
-        A_r = np.abs(Efields)
-        phi_r: NDArray[np.float64] = np.angle(Efields)  # type: ignore
+        # Noise-less current.
+        current = super().__call__(Efields)
 
-        i1 = self.photodiode_current(A_r, phi_r, 0)
-        i2 = self.photodiode_current(A_r, phi_r, np.pi)
-        i3 = self.photodiode_current(A_r, phi_r, np.pi / 2)
-        i4 = self.photodiode_current(A_r, phi_r, np.pi * 3 / 2)
+        # σ2 = 2*e*r*B_N where r is the photocurrent (approximately R*P_LO).
+        # Take the noise bandwidth as the Nyquist frequency.
+        B_n = self.sampling_rate / 2
+        noise_stdev = np.sqrt(
+            2 * elementary_charge * self.responsivity * self.lo_power * B_n
+        )
+        noise_r = self.rng.normal(0, noise_stdev, size=current.size)
+        noise_i = self.rng.normal(0, noise_stdev, size=current.size)
 
-        # Compute balanced photodetectors and pack into complex numbers.
-        i = np.empty_like(Efields, np.cdouble)
-        i.real = i1 - i2  # In-phase.
-        i.imag = i3 - i4  # Quadrature.
+        current += noise_r
+        current += 1j * noise_i
 
-        return i
+        return current
